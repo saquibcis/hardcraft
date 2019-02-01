6d8dd909a5422bf6e1ffa84ae0222107d58e2dfa
Mohammed Saquib Iqubal <Mohammed_Iqubal@ad.infosys.com>
2019-02-01 19:20:36 +0530

    second commit

--- js/main.js
+++ js/main.js
@@ -321,11 +321,10 @@ $(document).ready(function(){
                 // Basic options for a simple Google Map
                 // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
                 var mapOptions = {
-                    // How zoomed in you want the map to start at (always required)
+                    
                     zoom: 11,
 
-                    // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(40.6700, -73.9400), // New York
+                    center: new google.maps.LatLng(30.715152, 76.828965), 
 
                     // How you would like to style the map. 
                     // This is where you would paste any style found on Snazzy Maps.
@@ -341,7 +340,7 @@ $(document).ready(function(){
 
                 // Let's also add a marker while we're at it
                 var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(40.6700, -73.9400),
+                    position: new google.maps.LatLng(30.715152, 76.828965),
                     map: map,
                     title: 'Snazzy!'
                 });
